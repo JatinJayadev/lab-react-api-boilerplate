27128ac81d79c25193daf3debed09d3fd38da63f
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-19 22:10:50 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [books, setBooks] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://reactnd-books-api.udacity.com/books", { headers: { Authorization: "whatever-you-want" } });
+        setBooks(response.data.books)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, [])
+  console.log(books)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {books?.map((data) => (
+          <div key={data.id} id='main-container'>
+            <h2>{data.title}</h2>
+
+            <div id='box'>
+              <div id='img-des'>
+
+                <div>
+                  <img src={data.imageLinks.thumbnail} alt="" />
+                </div>
+
+                <div>
+                  <p id='description' >{data.description}</p>
+                </div>
+
+              </div>
+
+              <h3>{data.authors.join(", ")}</h3>
+            </div>
+
+            <hr />
+
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
